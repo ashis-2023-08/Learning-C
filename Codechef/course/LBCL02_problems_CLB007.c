c30474565276baf2be268af936022164e3b9ca61
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:12:41 +0530

    my fifth code

---
+++ Codechef/course/LBCL02_problems_CLB007.c
@@ -0,0 +1,13 @@
+// Write a program which does the following
+
+// Add a printf statement and output the sum of 3 + 4
+// Add another printf statement and output the sum of 2 + 1.
+// Use the printf syntax we learned in last problem.
+#include <stdio.h>
+
+int main() {
+    // Add two printf statements here
+    printf("%d%d\n", 3 + 4, 2 + 1);
+
+    return 0;
+}
