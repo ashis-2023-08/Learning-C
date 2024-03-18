92e3418c25161224b76958cf269cd80a3f198761
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 02:26:10 +0530

    My tenth practice code

---
+++ Codechef/practice/PCPP05_problems_WATERCONS.c
@@ -0,0 +1,25 @@
+// Water Consumption
+// Recently, Chef visited his doctor. The doctor advised Chef to drink at least 
+// 2000
+// 2000 ml of water each day.
+// Chef drank X ml of water today. Determine if Chef followed the doctor's advice or not.
+// Input Format
+// The first line contains a single integer T — the number of test cases. Then the test cases follow.
+// The first and only line of each test case contains one integer X — the amount of water Chef drank today.
+// Output Format
+// For each test case, output YES if Chef followed the doctor's advice of drinking at least 2000
+// 2000 ml of water. Otherwise, output NO.You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        
+    }
+	return 0;
+}
