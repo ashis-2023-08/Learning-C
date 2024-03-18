49709b4b53b9c63e08fbcea158ac98a400c40575
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:21:16 +0530

    My eight commit

---
+++ Codechef/course/LBCL02_problems_CLB011.c
@@ -0,0 +1,12 @@
+// Rearrange Code
+// Rearrange the code to produce this output:
+// #include <stdio.h>
+//   printf("World");
+// int main() {
+//   printf("Hello\n");
+// }
+#include <stdio.h>
+int main() {
+  printf("Hello\n");
+  printf("World");
+}
