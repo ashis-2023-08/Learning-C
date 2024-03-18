4fb51ea48205679fabdfc0813ae535984bd3a1b1
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 00:06:29 +0530

    My first practice code

---
+++ Codechef/PCPP01_problems_START01.c
@@ -0,0 +1,13 @@
+// Write a program that takes a number N as the input, and prints it to the output.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+    std::cin >> N;
+    std::cout << N << std::endl;
+    return 0;
+
+}
