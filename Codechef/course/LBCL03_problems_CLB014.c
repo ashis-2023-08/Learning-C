9c0e85a8b2f45366f033d2932abc4e615485f328
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:27:50 +0530

    My tenth code

---
+++ Codechef/course/LBCL03_problems_CLB014.c
@@ -0,0 +1,9 @@
+// Write a program which prints "7 and 3", using multiple %d in a single printf statement.
+#include <stdio.h>
+
+int main() {
+    // Add printf to output "7 and 3"
+    printf("%d and %d\n", 7, 3);
+    
+    return 0;
+}
