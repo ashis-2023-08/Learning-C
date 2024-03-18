0c6eebe84e92629880a38fbcc0797db7f2a7c30f
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:05:15 +0530

    my fourth code

---
+++ Codechef/course/LBCL01_problems_CLB003.c
@@ -0,0 +1,11 @@
+//Printing text
+// Replace the __ (underscores) in the editor with the text "I love C" to output I love C.
+
+#include <stdio.h>
+
+int main() {
+
+// Replace the _ (underscores) with the correct value
+printf("I love C");
+
+}
