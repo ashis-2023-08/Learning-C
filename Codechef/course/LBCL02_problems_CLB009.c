8e98f975a24a71d003310c09079da6155c1acb71
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:17:38 +0530

    my seventh code

---
+++ Codechef/course/LBCL02_problems_CLB009.c
@@ -0,0 +1,19 @@
+// Area & Perimeter of Rectangle
+// Write a program for the following problem
+
+// Let’s consider a rectangle of sides 11 and 13.
+// Find and output area of the rectangle having sides as 11 and 13
+// Find and output perimeter of the rectangle having side as 11 and 13
+// For any rectangle, the formula for area is length * breadth.
+// // The formula for perimeter is 2 * (length + breadth)
+#include <stdio.h>
+
+int main() {
+    // Add the formula for area of 11 and 13
+	printf("%d\n", 11 * 13);
+	
+	// Add the formula for perimeter of 11 and 13
+	printf("%d\n", 2 * (11 + 13));
+
+    return 0;
+}
