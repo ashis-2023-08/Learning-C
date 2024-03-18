d5ce9479ff80d61283e7869c09502e611072f0ce
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:15:05 +0530

    my six code

---
+++ Codechef/course/LBCL02_problems_CLB008.c
@@ -0,0 +1,13 @@
+// What to do if we want to print 20 and 40 on separate lines?
+
+// // We can use "\n" to have the next outputs be on the next line. It is like pressing the "Enter" / "Return" key on your keyboard, but for the output displayed.
+#include <stdio.h>
+
+int main() {
+    
+    // add %d along with \n to print on two different lines
+	printf("%d\n", 3 + 4);
+	printf("%d\n", 1 + 2);
+	return 0;
+
+}
