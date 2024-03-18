c9d49510e156534f530c1e62d394c7a51dfdbea9
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 01:58:04 +0530

    My second practice code

---
+++ Codechef/practice/PCPP01_problems_PRACREARR1.c
@@ -0,0 +1,23 @@
+// You want to input two integers, and output their sum.
+
+// Rearrange the code givens, so that it does the above task.
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a;
+//   cout << sum;
+//   int sum = a + b;
+//   cin >> a >> b;
+//   int b;
+//   return 0;
+// }
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a;
+  int b;
+  cin >> a >> b;
+  int sum = a + b;
+  cout << sum;
+  return 0;
+}
