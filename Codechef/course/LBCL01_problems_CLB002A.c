3d7bb468d927d57286f5ac24fc408e56bf7ff601
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 00:29:56 +0530

    My second code

---
+++ Codechef/course/LBCL01_problems_CLB002A.c
@@ -0,0 +1,16 @@
+/* Printing a number
+In the last problem we saw how to print a number. Now it's your turn to print another number.
+
+Your task is to print the number 20 on the screen.
+
+In the editor, I have written some code for you. But instead of number 20, __ (underscores) are written.
+
+Delete those underscores and write 20 at that place to complete the code. */
+#include <stdio.h>
+
+int main() {
+    
+    // Delete _ (underscore) and add 20 at it's place 
+    printf("%d", 20);
+    return 0;
+}
