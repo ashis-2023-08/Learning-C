6db85c3044cee809e2cc1a5803245179c30e7ce9
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 00:31:15 +0530

    My first practice code

---
+++ Codechef/practice/PCPP01_problems_START01.c
@@ -0,0 +1,13 @@
+// Number mirror
+//Write a program that takes a number N as the input, and prints it to the output.
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int N;
+    scanf("%d", &N); // Read the input number
+    printf("%d", N); // Print the input number
+    return 0;
+}
