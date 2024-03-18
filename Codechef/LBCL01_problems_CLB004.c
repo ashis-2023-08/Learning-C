43e2ce5f93f8285bde5b726f6a08e602064f96e9
AshisKumarRam <ashiskumar_ram@outlook.com>
2024-03-19 00:03:27 +0530

    My third code

---
+++ Codechef/LBCL01_problems_CLB004.c
@@ -0,0 +1,9 @@
+// Try to add 21 and 40 in code and print the result. Remember, we don't need "" (double quotes) when printing numbers.
+#include <stdio.h>
+
+int main() {
+
+  // Replace the underscores
+  printf("%d", 21 + 40);
+
+}
